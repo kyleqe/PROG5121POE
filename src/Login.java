f2c941a9477f72455a8589cfb8f478a883373bbd
KYLE HOVE <kylehove222@gmail.com>
2026-09-16 18:28:52 +0200

    Added login java file

---
+++ src/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prog5121poe;
+
+/**
+ *
+ * @author Kylen
+ */
+public class PROG5121POE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
