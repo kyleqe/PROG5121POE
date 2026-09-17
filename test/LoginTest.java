5550df62074ea5035d8bac9ff3d7d244dcc9f874
KYLE HOVE <kylehove222@gmail.com>
2026-09-17 18:57:19 +0200

    Added testCorrectUsername

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Kylen
- */
 public class LoginTest {
+    
+     @Test
+    public void testCorrectUsername() {
+        Login user = new Login("Kyle", "Hove", "kyl_1",
+                "Ch&&sec@ke99!", "+27838968976");
+        assertEquals(true, user.checkUserName());
+    }
+    
+    
     
 }
