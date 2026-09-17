46653d90cb3b0ab6a97772b6926fa4d04ecb060a
KYLE HOVE <kylehove222@gmail.com>
2026-09-17 14:54:03 +0200

    Added Public Login

---
+++ src/Login.java
@@ -0,0 +1,18 @@
+public class Login {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String loginUsername;
+    private String loginPassword;
+    
+     public Login(String firstName, String lastName, String username,
+                 String password, String cellPhoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+}
