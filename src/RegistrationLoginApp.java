362b715ad1485814684cbce0cf4841c7b6fa8ffb
KYLE HOVE <kylehove222@gmail.com>
2026-09-17 16:26:55 +0200

    Added User input

--- src/RegistrationLoginApp.java
+++ src/RegistrationLoginApp.java
@@ -2,4 +2,27 @@ import java.util.Scanner
 
 public class RegistrationLoginApp {
     
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        //user input
+        System.out.println();
+        System.out.println("       REGISTRATION AND LOGIN          ");
+        System.out.println();
+        
+        System.out.print("Enter your first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Enter a username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter a password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter your South African cell phone number: ");
+        String cellPhoneNumber = input.nextLine();
+
+    }
 }
